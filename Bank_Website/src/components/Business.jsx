41aca6cb42cf9f1db9378a94151fbfa20934ee53
Josh Erik <jseriki21@gmail.com>
2023-02-13 18:55:06 +0100

    Business section completed

--- Bank_Website/src/components/Business.jsx
+++ Bank_Website/src/components/Business.jsx
@@ -7,10 +7,20 @@ const FeatureCard = ({ icon, title, content, index }) => (
   <div
     className={`flex flex-row p-6 rounded-[20px] ${
       index !== features.length - 1 ? "mb-6" : "mb-0"
-    } feature card`}
+    } feature-card`}
   >
-    <div>
+    <div
+      className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}
+    >
       <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
+    </div>
+    <div className="flex-1 flex flex-col ml-3">
+      <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
+        {title}
+      </h4>
+      <p className="font-poppins  font-normal text-dimWhite text-[16px] leading-[24px] mb-1">
+        {content}
+      </p>
     </div>
   </div>
 );
@@ -26,15 +36,15 @@ const Business = () => (
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
         With the right credit card, you can improve your financial life by
         building credit, earning rewards and saving money. But with hundreds of
-        credit cards on the market.
+        credit cards on the market .
       </p>
       <Button styles="mt-10" />
+    </div>
 
-      <div className={`${layout.sectionImg} flex-col`}>
-        {features.map((feature, index) => {
-          <FeatureCard key={feature.id} {...feature} index={index} />;
-        })}
-      </div>
+    <div className={`${layout.sectionImg} flex-col`}>
+      {features.map((feature, index) => (
+        <FeatureCard key={feature.id} {...feature} index={index} />
+      ))}
     </div>
   </section>
 );
