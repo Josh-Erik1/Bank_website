e70894a5bd3754a3d7cb28e66dca1104468817eb
Josh Erik <jseriki21@gmail.com>
2023-02-13 13:00:19 +0100

    Business section

--- Bank_Website/src/components/Hero.jsx
+++ Bank_Website/src/components/Hero.jsx
@@ -4,10 +4,7 @@ import { discount, robot } from "../assets";
 import GetStarted from "./GetStarted";
 
 const Hero = () => (
-  <section
-    id="Home"
-    className={`flex md: flex-row flex-col ${styles.paddingY}`}
-  >
+  <section id="Home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
     <div
       className={`flex-1 ${styles.flexStart} flex-col xl:px-0  sm:px-16 px-6`}
     >
@@ -38,8 +35,19 @@ const Hero = () => (
         iusto quos.
       </p>
     </div>
-    <div>
-      <img src={robot} alt="billing" className="w-[100%] h-[100%] z-[5]" />
+    <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my 10 relative`}>
+      <img
+        src={robot}
+        alt="billing"
+        className="w-[100%] h-[100%] relative z-[5]"
+      />
+
+      <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+      <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient" />
+      <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 __gradient" />
+    </div>
+    <div className={`ss:hidden ${styles.flexCenter} `}>
+      <GetStarted />
     </div>
   </section>
 );
